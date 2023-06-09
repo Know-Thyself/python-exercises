b6bddc29dcdd5158f3c7f7e3033c0fa6d090ad71
Biruk Kebede <biruk.key@gmail.com>
2023-06-09 03:00:35 +0100

    further operations if the user wants to continue

--- python_dictionary/mini_calculator/mini_calculator.py
+++ python_dictionary/mini_calculator/mini_calculator.py
@@ -1,3 +1,7 @@
+from art import logo
+print(logo)
+print('Welcome to python mini calculator!')
+
 def add(x, y):
     return x + y
 
@@ -19,12 +23,21 @@ operations = {
 
 number1 = int(input('Enter a number\n'))
 number2 = int(input('Enter a second number\n'))
-for op in operations:
-    print(op)
-operation = input('Enter one of the above operation symbols to add, subtract, multiply or divide respectively\n')
 
 def calculate(n1, n2, symbol):
-    return operations[symbol](n1, n2)
+        return operations[symbol](n1, n2)
 
-answer = calculate(number1, number2, operation)
-print(f'{number1} {operation} {number2} = {answer}')
+is_continuous = True
+while is_continuous:
+    for op in operations:
+        print(op)
+    operation = input('Enter one of the above operation symbols to add, subtract, multiply or divide respectively\n')
+    answer = calculate(number1, number2, operation)
+    print(f'{number1} {operation} {number2} = {answer}')
+    more_calculations = input('Would you like to do more calculation? Type "yes" to continue or "no" to exit\n')
+    if more_calculations == 'no':
+        is_continuous = False
+    else:
+        number1 = answer
+        number2 = int(input('Enter a number\n'))
+
