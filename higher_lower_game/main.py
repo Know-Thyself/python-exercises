595468a04dbf667a25d4af9c0cbf689c36d5c184
Biruk Kebede <biruk.key@gmail.com>
2023-06-10 22:48:55 +0100

    repeating the game until the user wants to exit

--- higher_lower_game/main.py
+++ higher_lower_game/main.py
@@ -3,30 +3,41 @@ from data import game_data
 import random
 
 
-def game_setup():
-    print(logo)
-    first_option = random.choice(game_data)
-    person1 = format_entries(first_option)
-    print(f"Compare A: {person1}")
-    print(vs)
+def game_setup(first_option=random.choice(game_data), score=0):
     second_option = random.choice(game_data)
-    person2 = format_entries(second_option)
-    print(f"Against B: {person2}")
-    answer = input("Who has more instagram followers? Type 'A' or 'B': ")
-    if answer == "A":
-        evaluate_answer(first_option, second_option)
-    elif answer == "B":
-        evaluate_answer(second_option, first_option)
+    print(logo)
+    print(f"Compare A: {format_entries(first_option)}")
+    print(vs)
+    print(f"Against B: {format_entries(second_option)}")
+    play(first_option, second_option, score)
 
 
 def format_entries(entry):
     return f"{entry['name']}, {entry['description']}, {entry['country']}"
 
-def evaluate_answer(entry1, entry2):
-    if entry1['follower_count'] > entry2['follower_count']:
-        print('Correct!')
+
+def play(entry1, entry2, score=0):
+    print(f"Your current score is: {score}")
+    answer = input("Who has more instagram followers? Type 'A' or 'B': ")
+    if answer == "A":
+        result = evaluate_answer(entry1, entry2, score)
+    elif answer == "B":
+        result = evaluate_answer(entry2, entry1, score)
+    if result:
+        current_score = score + 1
+        game_setup(entry2, current_score)
     else:
-        print('Incorrect!')
+        if input('Type "y" if you want to play again or "n" to exit: ') == "y":
+            game_setup()
+
+
+def evaluate_answer(entry1, entry2, score=0):
+    if entry1["follower_count"] > entry2["follower_count"]:
+        print(f"You're right! Your current score is: {score + 1}")
+        return True
+    else:
+        print(f"Incorrect! Your score was {score}")
+        return False
 
 
 game_setup()
