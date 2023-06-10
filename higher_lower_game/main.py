319e8ab1cbb2ea85d6128cd77917c69f84e58ced
Biruk Kebede <biruk.key@gmail.com>
2023-06-11 00:07:28 +0100

    improved variable naming and user experience

--- higher_lower_game/main.py
+++ higher_lower_game/main.py
@@ -1,42 +1,49 @@
 from ascii_art import logo, vs
-from data import game_data
+from data import accounts
 import random
 
 
-def game_setup(first_option=random.choice(game_data), score=0):
-    second_option = random.choice(game_data)
+def game_setup(account1=random.choice(accounts), score=0):
+    account2 = random.choice([i for i in accounts if i != account1])
     print(logo)
-    print(f"Compare A: {format_entries(first_option)}")
+    print(f"Compare A: {format_entries(account1)}")
     print(vs)
-    print(f"Against B: {format_entries(second_option)}")
-    play(first_option, second_option, score)
+    print(f"Against B: {format_entries(account2)}")
+    play(account1, account2, score)
 
 
-def format_entries(entry):
-    return f"{entry['name']}, {entry['description']}, {entry['country']}"
+def format_entries(account):
+    return f"{account['name']}, {account['description']}, {account['country']}"
 
 
 def play(entry1, entry2, score=0):
     print(f"Your current score is: {score}")
-    answer = input("Who has more instagram followers? Type 'A' or 'B': ")
-    if answer == "A":
+    answer = input("Who has more instagram followers? Type 'A/a' or 'B/b': ").lower()
+    if answer == "a":
         result = evaluate_answer(entry1, entry2, score)
-    elif answer == "B":
+    elif answer == "b":
         result = evaluate_answer(entry2, entry1, score)
     if result:
         current_score = score + 1
         game_setup(entry2, current_score)
     else:
-        if input('Type "y" if you want to play again or "n" to exit: ') == "y":
+        shall_play_again = input(
+            'Type "Y/y" if you want to play again or "N/n" to exit: '
+        ).lower()
+        if shall_play_again == "y":
             game_setup()
 
 
 def evaluate_answer(entry1, entry2, score=0):
     if entry1["follower_count"] > entry2["follower_count"]:
-        print(f"You're right! Your current score is: {score + 1}")
+        print(
+            f"You're right! {entry1['name']} has more followers than {entry2['name']}."
+        )
         return True
     else:
-        print(f"Incorrect! Your score was {score}")
+        print(
+            f"Incorrect! {entry2['name']} has more followers. \nYour score was {score}"
+        )
         return False
 
 
