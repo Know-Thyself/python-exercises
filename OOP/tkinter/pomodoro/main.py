9b43520d6b47168e010dc3453f7741fbfd439b41
Biruk Kebede <biruk.key@gmail.com>
2023-06-20 16:19:37 +0100

    error handling by try and except method

--- OOP/tkinter/pomodoro/main.py
+++ OOP/tkinter/pomodoro/main.py
@@ -5,55 +5,55 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 1
+SHORT_BREAK_MIN = 1
+LONG_BREAK_MIN = 1
 CHECKMARK = "✅"
 
-keep_timer_going = True
+timer_id = None
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-def countdown_with_timeout(sec, is_working_hour):
-    if keep_timer_going:
-        if sec == 0 and is_working_hour:
-            completed_cycles["text"] += CHECKMARK
-            completed_rounds = len(completed_cycles["text"])
-            if completed_rounds % 4 == 0:
-                print('4 x 25 minutes work done. Enjoy your 20 minutes break!')
-                long_break()
-            else:
-                print('25 minutes Completed. Enjoy your 5 minutes break!')
-                short_break()
-        elif sec == 0 and not is_working_hour:
-            countdown()
-        minutes, seconds = divmod(sec, 60)
-        timer = '{:02d}:{:02d}'.format(minutes, seconds)
-        print(f"00:{timer}")
-        canvas.itemconfig(canvas_text, text=f"00:{timer}")
-        if sec > 0:
-            root.after(1000, countdown_with_timeout, sec - 1, is_working_hour)
+def countdown(sec, is_working_hour):
+    if sec == 0 and is_working_hour:
+        completed_cycles["text"] += CHECKMARK
+        completed_rounds = len(completed_cycles["text"])
+        if completed_rounds % 4 == 0:
+            label["text"] = "Long Break"
+            long_break()
+        else:
+            label["text"] = "Short Break"
+            short_break()
+    elif sec == 0 and not is_working_hour:
+        label["text"] = "Timer"
+        start_countdown()
+    minutes, seconds = divmod(sec, 60)
+    timer = '{:02d}:{:02d}'.format(minutes, seconds)
+    canvas.itemconfig(canvas_text, text=f"00:{timer}")
+    if sec > 0:
+        global timer_id
+        timer_id = root.after(1000, countdown, sec - 1, is_working_hour)
 
 
 def short_break():
     seconds = SHORT_BREAK_MIN * 60
-    countdown_with_timeout(seconds, False)
+    countdown(seconds, False)
 
 
 def long_break():
     seconds = LONG_BREAK_MIN * 60
-    countdown_with_timeout(seconds, False)
+    countdown(seconds, False)
 
 
-def countdown():
-    time_in_seconds = WORK_MIN * 60
-    countdown_with_timeout(time_in_seconds, True)
+def start_countdown():
+    seconds = WORK_MIN * 60
+    countdown(seconds, True)
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
-    global keep_timer_going
-    keep_timer_going = False
+    root.after_cancel(timer_id)
+    label["text"] = "Timer"
     canvas.itemconfig(canvas_text, text="00:00:00")
     completed_cycles["text"] = ""
 
@@ -71,10 +71,10 @@ canvas_text = canvas.create_text(102, 132, text="00:00:00", font=(FONT_NAME, 24,
 canvas.pack()
 
 # Buttons
-start_countdown = tk.Button(root, text="Start", font=(FONT_NAME, 16, "bold"), bg="red", fg="white", command=countdown)
-start_countdown.pack(padx=20, ipadx=10, pady=10, side=tk.LEFT, fill="x")
-reset_timer = tk.Button(text="Reset", font=(FONT_NAME, 16, "bold"), command=reset_timer)
-reset_timer.pack(padx=20, ipadx=10, pady=20, side=tk.RIGHT, fill="x")
+start = tk.Button(root, text="Start", font=(FONT_NAME, 16, "bold"), bg=GREEN, fg="white", command=start_countdown)
+start.pack(padx=20, ipadx=10, pady=10, side=tk.LEFT, fill="x")
+reset = tk.Button(text="Reset", font=(FONT_NAME, 16, "bold"), command=reset_timer, bg=RED, fg=YELLOW)
+reset.pack(padx=20, ipadx=10, pady=20, side=tk.RIGHT, fill="x")
 
 # Label
 cycles = tk.Label(text="Completed Cycles:", font=(FONT_NAME, 16, "bold"), fg=GREEN)
--- nato_phonetic_alphabet/main.py
+++ nato_phonetic_alphabet/main.py
@@ -1,10 +1,22 @@
 import pandas
 
-alphabet_df = pandas.read_csv("nato_phonetic_alphabet.csv")
-name = list(input("What is your name: "))
-new_dict = {row.letter: row.code for (index, row) in alphabet_df.iterrows()}
-nato_phonetic_name = [new_dict[letter.upper()] for letter in name if letter != ' ']
-print(new_dict)
 
-print(nato_phonetic_name)
+def generate_nato_phonetic_words():
+    alphabet_df = pandas.read_csv("nato_phonetic_alphabet.csv")
+    name = list(input("What is your name: "))
+    new_dict = {row.letter: row.code for (index, row) in alphabet_df.iterrows()}
+    try:
+        nato_phonetic_name = [new_dict[letter.upper()] for letter in name if letter != ' ']
+    except KeyError:
+        print("Sorry, only names with English alphabet can be used")
+        generate_nato_phonetic_words()
+    else:
+        print(nato_phonetic_name)
+    finally:
+        more_names = input("Would you like to go again? Type 'y' for yes or 'n' for no: ")
+        if more_names == "y":
+            generate_nato_phonetic_words()
+
+
+generate_nato_phonetic_words()
 
