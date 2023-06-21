3f20d3ed63e5b50cab3af532fc4bcaa9e24a4613
Biruk Kebede <biruk.key@gmail.com>
2023-06-22 00:53:09 +0100

    refactored web scraping and class constracting

--- web_scraping/all_time_100_movies/main.py
+++ web_scraping/all_time_100_movies/main.py
@@ -8,40 +8,27 @@ soup = BeautifulSoup(response, "html.parser")
 movies_soup = soup.find_all(class_="_h3_cuogz_1")
 images_soup = soup.find_all(name="img")
 summaries_soup = soup.find_all(class_="_summary_kc5qn_21")
+images = [
+    img.get("src") for img in images_soup[1:] if not img.get("src").startswith("data")
+]
 
-titles = []
-images = []
-summaries = []
-list_of_dict = []
+movies = []
 
-for movie in movies_soup:
-    titles.append(movie.text)
-
-for image in images_soup:
-    image_src = image.get("src")
-    if not image_src.startswith("data"):
-        images.append(image_src)
-
-for summary in summaries_soup:
-    summaries.append(summary.text)
-
-# Generating dictionaries of movies
 for i in range(100):
-    temp_list = titles[i].split()
+    temp_list = movies_soup[i].text.split()
     title = " ".join(temp_list[1:])
-    image = images[1:][i]
     rank = int(temp_list[0].replace(".", ""))
-    summary = summaries[i]
+    image = images[i]
+    summary = summaries_soup[i].text
     movie = Movie(i + 1, title, image, rank, summary)
-    movie_obj = movie.get_movie_object()
-    list_of_dict.append(movie_obj)
+    movies.append(movie.description)
 
-json_object = json.dumps(list_of_dict, indent=4)
+json_object = json.dumps(movies, indent=4)
 with open("movies.json", "w") as file:
     file.write(json_object)
 
 # Writing a text file
 write_text_file = open("movies.txt", "w")
-for movie in list_of_dict:
+for movie in movies:
     line = f"{movie['rank']}. {movie['title']}"
     write_text_file.write(f"{line}\n")
--- web_scraping/all_time_100_movies/movie.py
+++ web_scraping/all_time_100_movies/movie.py
@@ -1,16 +1,12 @@
 class Movie:
     def __init__(self, m_id, title, img, rank, summary):
-        self.id = m_id
-        self.title = title
-        self.image = img
-        self.rank = rank
-        self.summary = summary
-
-    def get_movie_object(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "image": self.image,
-            "rank": self.rank,
-            "summary": self.summary,
+        self.description = {
+            "id": m_id,
+            "title": title,
+            "image": img,
+            "rank": rank,
+            "summary": summary,
         }
+
+    def get_movie(self):
+        return self.description
