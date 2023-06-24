0318b348c1342392918a96032fc796eb7876dd16
Biruk Kebede <biruk.key@gmail.com>
2023-06-24 12:49:23 +0100

    automating google form filling

--- web_scraping/billboard/main.py
+++ web_scraping/billboard/main.py
@@ -2,8 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import csv
+import os
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
-date = input("Which day do you want to travel back in time? Please use this format YYYY-MM-DD: ")
+load_dotenv()
+
+date = input(
+    "Which day do you want to travel back in time? Please use this format YYYY-MM-DD: "
+)
 
 response = requests.get(f"https://www.billboard.com/charts/hot-100/{date}").text
 soup = BeautifulSoup(response, "html.parser")
@@ -15,7 +24,11 @@ songs = []
 # Extracting and writing text file
 text = open("billboard.txt", "w")
 for i in range(len(titles)):
-    rank_and_song = {"rank": i + 1, "song": titles[i].text.strip(), "artist": artists[i].text.strip()}
+    rank_and_song = {
+        "rank": i + 1,
+        "song": titles[i].text.strip(),
+        "artist": artists[i].text.strip(),
+    }
     songs.append(rank_and_song)
     text.write(f"{rank_and_song['rank']}. {rank_and_song['song']}\n")
 
@@ -26,7 +39,42 @@ with open("billboard.json", "w") as json_file:
 
 # Writing csv file
 field_names = list(songs[0].keys())
-with open('billboard.csv', 'w', encoding='UTF8', newline='') as csv_file:
+with open("billboard.csv", "w", encoding="UTF8", newline="") as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=field_names)
     writer.writeheader()
     writer.writerows(songs)
+
+# Filling google form with selenium
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+options.add_argument("-incognito")
+options.add_experimental_option("excludeSwitches", ['enable-automation']);
+driver = webdriver.Chrome(options=options)
+url = os.getenv("GOOGLE_FORM_URL")
+
+for song in songs:
+    driver.get(url)
+    time.sleep(1)
+    rank_input = driver.find_element(
+        By.XPATH,
+        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input',
+    )
+
+    song_input = driver.find_element(
+        By.XPATH,
+        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input',
+    )
+    artist_input = driver.find_element(
+        By.XPATH,
+        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input',
+    )
+    submit = driver.find_element(
+        By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span'
+    )
+    rank_input.send_keys(song["rank"])
+    song_input.send_keys(song["song"])
+    artist_input.send_keys(song["artist"])
+    submit.click()
+    reload_from = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
+    reload_from.click()
+    time.sleep(1)
--- web_scraping/selenium-driver/interacting.py
+++ web_scraping/selenium-driver/interacting.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
@@ -11,12 +12,13 @@ search_bar = driver.find_element(By.ID, "searchInput")
 # Typing on searchbar
 search_bar.send_keys("python programming")
 icon_button = driver.find_element(By.CSS_SELECTOR, ".pure-button .svg-search-icon")
-icon_button.click()
+search_bar.send_keys(Keys.ENTER)
+# icon_button.click()
 
-url = "https://www.python.org/"
-driver.get(url)
-python_docs = driver.find_element(By.LINK_TEXT, "Docs")
+# url = "https://www.python.org/"
+# driver.get(url)
+# python_docs = driver.find_element(By.LINK_TEXT, "Docs")
 # Clicking an anchor tag/button
-python_docs.click()
+# python_docs.click()
 
 # driver.quit()
