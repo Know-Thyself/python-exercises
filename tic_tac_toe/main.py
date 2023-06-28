8027f75df5de163aea9b43ad0e7a05e45ad1ff37
Biruk Kebede <biruk.key@gmail.com>
2023-06-28 03:34:34 +0100

    tic-tac-toe: my implementation steps

---
+++ tic_tac_toe/main.py
@@ -0,0 +1,120 @@
+# Global variable
+current_board = [' '] * 10
+
+
+def welcome_display():
+    board_chars = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+    print('Welcome to Tic Tac Toe Board Game!')
+    display_board(board_chars)
+    game_setup(current_board)
+
+
+def game_setup(board):
+    print('Current Board:')
+    display_board(board)
+    player_input()
+
+
+def display_board(board):
+    rows = [board[1:4], board[4:7], board[7:]]
+    list(map(display_row, rows))
+
+
+def display_row(row):
+    for index, item in enumerate(row):
+        if index == 0:
+            print('|' + item + '|', end="")
+        elif index == 1:
+            print(item + '|', end="")
+        else:
+            print(item + '|')
+
+
+def player_input():
+    global current_board
+    # Player one gets to choose marker
+    player_one, player_two = validate_player_marker()
+    # Player two gets to make the first move
+    player_two_position = validate_players_move(player_two)
+    current_board[player_two_position] = player_two
+    display_board(current_board)
+    next_player(player_one)
+
+
+def validate_player_marker():
+    player_one = ''
+    player_two = ''
+    while player_one not in ['X', 'O']:
+        player_one = input('Please enter "X" or "O" as your marker: ').upper()
+        if player_one == 'X':
+            player_two = 'O'
+        else:
+            player_two = 'X'
+    return player_one, player_two
+
+
+def validate_players_move(player):
+    player_position = 'Not a number'
+    while player_position not in range(1, 10):
+        player_position = input(
+            f'You are {player}. Please chose your position (1 - 9): ')
+        if player_position.isnumeric():
+            player_position = int(player_position)
+    return player_position
+
+
+def next_player(player):
+    global current_board
+    player_position = validate_players_move(player)
+    current_board[player_position] = player
+    display_board(current_board)
+    game_status(player)
+
+
+def game_status(player):
+    global current_board
+    # Check if either vertical, horizontal or diagonal lines values are identical
+    if does_win(current_board[1:4]) \
+            or does_win(current_board[4:7]) \
+            or does_win(current_board[7:]) \
+            or does_win(current_board[1:8:3]) \
+            or does_win(current_board[2:9:3]) \
+            or does_win(current_board[3::3]) \
+            or does_win(current_board[1::4]) \
+            or does_win(current_board[3:8:2]):
+        print(f'Player {player} wins!')
+        play_again = input('Enter "Y" to play again or "N" to exit: ').lower()
+        if play_again == 'y':
+            reset()
+    elif ' ' not in current_board[1:]:
+        print("It's a cat's game! No one wins.")
+        play_again = input('Enter "Y" to play again or "N" to exit: ').lower()
+        if play_again == 'y':
+            reset()
+    else:
+        if player == 'X':
+            next_player('O')
+        else:
+            next_player('X')
+
+
+def does_win(lst):
+    """
+    Takes in a list and checks if all values in a list are identical
+    :param lst:
+    :return: bool
+    """
+    if lst[0] != ' ':
+        return all(i == lst[0] for i in lst)
+    else:
+        return False
+
+
+def reset():
+    global current_board
+    current_board = [' '] * 10
+    game_setup(current_board)
+
+
+if __name__ == '__main__':
+    welcome_display()
